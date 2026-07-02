eae89eb2103793e3f9e8bed416619b700b96283d
kahlaoui <kahlaoui.ghaieth@gmail.com>
2026-07-02 22:54:39 +0200

    Reorganize style application in main function for better clarity and control

--- main.go
+++ main.go
@@ -14,7 +14,8 @@ func main() {
 		Add(chalk.Bold).
 		Add(chalk.Underline)
 	c.Println("Hello, World!")
-	c.Clear().RGB(255, 0, 0).Println("This is red text")
+	c.Disable().Println("Disabled styles")
+	c.Enable().Clear().RGB(255, 0, 0).Println("This is red text")
 	f, err := os.Create("test.txt")
 	if err != nil {
 		panic(err)
